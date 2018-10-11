df1c9978c61eb935e194f31f58523bbf793fcf0b
Victor Santos <vsant.professional@gmail.com>
2018-10-11 15:51:35 -0300

    inserção de usuário com POST

--- src/main/java/com/victorsantos/workshopmongodb/services/UserService.java
+++ src/main/java/com/victorsantos/workshopmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.victorsantos.workshopmongodb.domain.User;
+import com.victorsantos.workshopmongodb.dto.UserDTO;
 import com.victorsantos.workshopmongodb.repositories.UserRepository;
 import com.victorsantos.workshopmongodb.services.exceptions.ObjectNotFoundException;
 
@@ -24,5 +25,15 @@ public class UserService {
 	{
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) 
+	{
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) 
+	{
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
