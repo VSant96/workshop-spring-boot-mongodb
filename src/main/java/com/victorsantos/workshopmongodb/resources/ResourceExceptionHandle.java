b6730f4102805e0cb6156d8233f360b746d28c81
Victor Santos <vsant.professional@gmail.com>
2018-10-11 15:38:06 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/victorsantos/workshopmongodb/resources/ResourceExceptionHandle.java
@@ -0,0 +1,26 @@
+package com.victorsantos.workshopmongodb.resources;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.victorsantos.workshopmongodb.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandle {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		StandardError err = new StandardError(
+				System.currentTimeMillis(), 
+				HttpStatus.NOT_FOUND.value(), 
+				"Não encontrado",
+				e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(err);
+	}
+
+}
